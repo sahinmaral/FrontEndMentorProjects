ef03cc1dc2056dcfd48d6acee4b921e6d9e07446
sahinmaral <sahin.maral@hotmail.com>
2023-03-23 00:29:52 +0300

    Added Notification Page

---
+++ NotificationsPage/script.js
@@ -0,0 +1,175 @@
+const fetchNotifications = () => {
+  fetch("./notifications.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((notifications) => {
+      loadNotifications(notifications);
+    });
+};
+
+const markAsRead = () => {
+  let elements = document.querySelectorAll(
+    ".notifications > .notification-card.unread"
+  );
+  elements.forEach((element, index) => {
+    element.classList.remove("unread");
+
+    document.querySelectorAll(
+      ".notifications > .notification-card > .first-row > .notification-body > .notification-information> .unread-message"
+    )[index].style.display = "none";
+  });
+
+  document.querySelector(
+    ".notifications > .notification-header > .left-side > .notification-count"
+  ).innerHTML = 0;
+};
+
+const loadNotifications = (notifications) => {
+  let unreadCountElement = document.querySelector(
+    ".notifications > .notification-header > .left-side > .notification-count"
+  );
+
+  let notificationContent = document.querySelector(".notifications");
+
+  unreadCountElement.innerHTML = notifications.filter(
+    (notification) => notification.isUnread
+  ).length;
+
+  notifications.forEach((notification) => {
+    const notificationCard = document.createElement("div");
+    notificationCard.classList.add("notification-card");
+
+    const firstRow = document.createElement("div");
+    firstRow.classList.add("first-row");
+
+    const notificationThumbnail = document.createElement("div");
+    notificationThumbnail.classList.add("notification-thumbnail");
+
+    const thumbnailImage = document.createElement("img");
+    thumbnailImage.setAttribute("alt", notification.username);
+
+    const thumbnailImagePath =
+      "./assets/images/" + notification.thumbnail_image;
+    thumbnailImage.setAttribute("src", thumbnailImagePath);
+    notificationThumbnail.appendChild(thumbnailImage);
+
+    const notificationBody = document.createElement("div");
+    notificationBody.classList.add("notification-body");
+
+    const notificationInformation = document.createElement("div");
+    notificationInformation.classList.add("notification-information");
+
+    const notificationInformationSpan = document.createElement("span");
+
+    const notificationUsernameSpan = document.createElement("span");
+    notificationUsernameSpan.classList.add("username");
+    notificationUsernameSpan.innerHTML = notification.username;
+
+    notificationInformationSpan.appendChild(notificationUsernameSpan);
+
+    const notificationMessageSpan = document.createElement("span");
+    const notificationActionSpan = document.createElement("span");
+
+    firstRow.appendChild(notificationThumbnail);
+    firstRow.appendChild(notificationBody);
+    notificationCard.appendChild(firstRow);
+
+    notificationContent.appendChild(notificationCard);
+    notificationBody.appendChild(notificationInformation);
+
+    switch (notification.notification_type) {
+      case "REACT_POST":
+        notificationMessageSpan.innerHTML = "reacted to your recent post ";
+        notificationInformationSpan.appendChild(notificationMessageSpan);
+
+        notificationActionSpan.innerHTML = notification.action_detail;
+        notificationActionSpan.classList.add("post-name");
+
+        notificationInformationSpan.appendChild(notificationActionSpan);
+
+        break;
+      case "FOLLOW":
+        notificationMessageSpan.innerHTML = "followed you ";
+        notificationInformationSpan.appendChild(notificationMessageSpan);
+
+        break;
+      case "LEAVE_GROUP":
+        notificationMessageSpan.innerHTML = "left the group ";
+        notificationInformationSpan.appendChild(notificationMessageSpan);
+
+        notificationActionSpan.innerHTML = notification.action_detail;
+        notificationActionSpan.classList.add("group-name");
+
+        notificationInformationSpan.appendChild(notificationActionSpan);
+
+        break;
+      case "JOIN_GROUP":
+        notificationMessageSpan.innerHTML = "has joined your group ";
+        notificationInformationSpan.appendChild(notificationMessageSpan);
+
+        notificationActionSpan.innerHTML = notification.action_detail;
+        notificationActionSpan.classList.add("group-name");
+
+        notificationInformationSpan.appendChild(notificationActionSpan);
+
+        break;
+
+      case "PRIVATE_MESSAGE":
+        notificationMessageSpan.innerHTML = "sent you a private message ";
+        notificationInformationSpan.appendChild(notificationMessageSpan);
+
+        const secondRow = document.createElement("div");
+        secondRow.classList.add("second-row");
+
+        const notificationPrivateMessage = document.createElement("div");
+        notificationPrivateMessage.classList.add("notification-message");
+        notificationPrivateMessage.innerHTML = notification.action_detail;
+
+        secondRow.appendChild(notificationPrivateMessage);
+
+        notificationCard.appendChild(secondRow);
+
+        break;
+
+      case "COMMENT_PICTURE":
+        notificationCard.classList.add("liked-comment");
+
+        notificationMessageSpan.innerHTML = "commented on your picture ";
+        notificationInformationSpan.appendChild(notificationMessageSpan);
+
+        const firstRowImageDiv = document.createElement("div");
+        firstRowImageDiv.classList.add("first-row-image");
+        const firstRowImage = document.createElement("img");
+        firstRowImage.setAttribute("alt", "commented-picture");
+
+        const firstRowImagePath =
+          "./assets/images/" + notification.action_detail;
+        firstRowImage.setAttribute("src", firstRowImagePath);
+        firstRowImageDiv.appendChild(firstRowImage);
+
+        notificationCard.appendChild(firstRowImageDiv);
+
+        break;
+      default:
+        break;
+    }
+
+    notificationInformation.appendChild(notificationInformationSpan);
+
+    const notificationTimeline = document.createElement("p");
+    notificationTimeline.innerHTML = notification.createdAt;
+    notificationTimeline.classList.add("notification-timeline");
+    notificationBody.appendChild(notificationTimeline);
+
+    if (notification.isUnread) {
+      notificationCard.classList.add("unread");
+
+      const unReadMessageIconElement = document.createElement("div");
+      unReadMessageIconElement.classList.add("unread-message");
+      notificationInformation.appendChild(unReadMessageIconElement);
+    }
+  });
+};
+
+fetchNotifications();
---
+++ NotificationsPage/style-guide.md
@@ -0,0 +1,51 @@
+<style>
+    .color-palette{
+        width:300px;
+        height:50px;
+        border:1px solid black;
+        margin:10px;
+        text-align:center;
+        padding-top:25px;
+        color:red;
+        font-weight:900;
+    }
+</style>
+
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+<div class="color-palette" style="background-color:hsl(1, 90%, 64%)">Red: hsl(1, 90%, 64%)</div>
+<div class="color-palette" style="background-color:hsl(219, 85%, 26%)">Blue: hsl(219, 85%, 26%)</div>
+
+
+
+### Neutral
+
+<div class="color-palette" style="background-color:hsl(0, 0%, 100%)">White: hsl(0, 0%, 100%)</div>
+<div class="color-palette" style="background-color:hsl(210, 60%, 98%)">Very light grayish blue: hsl(210, 60%, 98%)</div>
+<div class="color-palette" style="background-color:hsl(211, 68%, 94%)">Light grayish blue 1: hsl(211, 68%, 94%)</div>
+<div class="color-palette" style="background-color:hsl(205, 33%, 90%)">Light grayish blue 2: hsl(205, 33%, 90%)</div>
+<div class="color-palette" style="background-color:hsl(219, 14%, 63%)">Grayish blue: hsl(219, 14%, 63%)</div>
+<div class="color-palette" style="background-color:hsl(219, 12%, 42%)">Dark grayish blue: hsl(219, 12%, 42%)</div>
+<div class="color-palette" style="background-color:hsl(224, 21%, 14%)">Very dark blue: hsl(224, 21%, 14%)</div>
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 16px
+
+### Font
+
+- Family: [Plus Jakarta Sans](https://fonts.google.com/specimen/Plus+Jakarta+Sans)
+- Weights: 500, 800
